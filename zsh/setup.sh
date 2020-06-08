12ca4606d3552cbbea516ecabba3a0ce1d6d683c
Andrea Vallotti <andrea.vallotti@gmail.com>
2020-06-08 18:41:21 +0200

    enable zsh autocompletion

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,9 +3,11 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 files=(
-	.zshrc .zsh_prompt .zsh_extra
+	.zshrc .zsh_prompt .zsh_extra .zsh_autocomplete
 )
 
 for file in ${files[@]}; do
 	[ -r "$file" ] && [ -f "$file" ] && ln -sfn ${BASEDIR}/"$file" ~/"$file";
 done;
+
+chmod -R 755 /usr/local/share/zsh
