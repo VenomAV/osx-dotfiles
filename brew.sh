eeff41cb95fc88b224e06050c6ab08353a0abb11
Andrea Vallotti <andrea.vallotti@gmail.com>
2020-10-01 17:38:44 +0200

    add betterzip + miro

--- brew.sh
+++ brew.sh
@@ -98,10 +98,11 @@ brew cask install docker
 brew cask install java
 brew cask install keybase
 brew cask install mongodb-compass-community
+brew cask install betterzip
+brew cask install miro
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo
-
 xattr -d -r com.apple.quarantine ~/Library/QuickLook #Remove quarantine attributes on Catalina and later
 
 brew cask install dotnet-sdk
