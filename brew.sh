0d59c054718f6ed913654a8c64bd2b699b7fc956
Andrea Vallotti <andrea.vallotti@gmail.com>
2024-02-29 09:41:16 +0100

    brew: add citrix-workspace

--- brew.sh
+++ brew.sh
@@ -138,6 +138,7 @@ brew install --cask spotify
 brew install --cask sublime-merge
 brew install --cask karabiner-elements
 brew install --cask authy
+brew install --cask citrix-workspace
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo
