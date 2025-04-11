6e3e5e22a3276f37d6ce3771731e9e1490eb32cb
Andrea Vallotti <andrea.vallotti@gmail.com>
2025-04-11 16:58:13 +0200

    install rustup

--- after-brew.sh
+++ after-brew.sh
@@ -17,3 +17,8 @@ pip3 install togglCli
 bash <(curl -L https://nixos.org/nix/install) --daemon
 mkdir -p ~/.config/nix
 ln -sfn ${BASEDIR}/.config/nix.conf ~/.config/nix/nix.conf
+
+if ! command -v rustup &> /dev/null
+then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
--- bash/.bash_profile
+++ bash/.bash_profile
@@ -22,3 +22,4 @@ done;
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 
 test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+. "$HOME/.cargo/env"
