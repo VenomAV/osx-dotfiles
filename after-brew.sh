9779922c12e721facf771c405b3ab9edffe0105e
Andrea Vallotti <andrea.vallotti@gmail.com>
2026-03-11 10:28:34 +0100

    zsh setup - install pnpm-shell-completion

--- after-brew.sh
+++ after-brew.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
-fnm install v20
-fnm install v22
-fnm install v24
-fnm default v24
-fnm use v24
+# fnm install v20
+# fnm install v22
+# fnm install v24
+# fnm default v24
+# fnm use v24
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p tmp
+curl -L https://github.com/g-plane/pnpm-shell-completion/releases/download/v0.5.5/pnpm-shell-completion_aarch64-apple-darwin.tar.gz -f -o tmp/pnpm-shell-completion.tar.gz
+mkdir -p tmp/pnpm-shell-completion
+tar -xzf ./tmp/pnpm-shell-completion.tar.gz -C ./tmp/pnpm-shell-completion
+cd ./tmp/pnpm-shell-completion && ./install.zsh "$ZSH/custom/plugins" && cd ../..
+rm -rf tmp
 
-ln -sfn ${BASEDIR}/terminal/starship.toml ~/.config/starship.toml
+# BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pip3 install togglCli
+# ln -sfn ${BASEDIR}/terminal/starship.toml ~/.config/starship.toml
 
-bash <(curl -L https://nixos.org/nix/install) --daemon
-mkdir -p ~/.config/nix
-ln -sfn ${BASEDIR}/.config/nix.conf ~/.config/nix/nix.conf
+# pip3 install togglCli
 
-if ! command -v rustup &> /dev/null
-then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
+# bash <(curl -L https://nixos.org/nix/install) --daemon
+# mkdir -p ~/.config/nix
+# ln -sfn ${BASEDIR}/.config/nix.conf ~/.config/nix/nix.conf
+
+# if ! command -v rustup &> /dev/null
+# then
+#     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# fi
